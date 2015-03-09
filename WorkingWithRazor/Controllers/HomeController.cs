650a4e59907ec8780d392a93a68f2b668806165b
DerekRivers <dyk3rz@googlemail.com>
2015-03-09 09:08:40 +0000

    MVC

---
+++ WorkingWithRazor/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace WorkingWithRazor.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            string[] names = {"Apple", "Orange", "Pear"};
+
+            return View(names);
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Time()
+        {
+            return PartialView(DateTime.Now);
+        }
+    }
+}
---
+++ WorkingWithRazor/Global.asax.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using WorkingWithRazor.Infrastructure;
+
+
+namespace WorkingWithRazor
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            ViewEngines.Engines.Add(new CustomLocationViewEngine());
+    
+        }
+    }
+}
